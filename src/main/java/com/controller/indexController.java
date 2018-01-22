5ed6d421ded82c92ee02f37a90d9dc6cb9b462d0
Shubham R Durugkar <shubham341.rd@gmail.com>
2018-01-22 15:30:25 +0530

    Integrated frontend with backend project..

--- src/main/java/com/controller/indexController.java
+++ src/main/java/com/controller/indexController.java
@@ -1,15 +1,24 @@
 package com.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.dao.UserDao;
 
 @Controller
 public class indexController {
-
-	 @RequestMapping("/")
-    public String indexPage(){
-		    return "index";
+@Autowired
+private UserDao userdao;
+	 @RequestMapping(value={"/","/index"})
+    public ModelAndView indexPage(){
+		 ModelAndView mv=new ModelAndView("index");
+		 mv.addObject("title","/index");  
+		 mv.addObject("User",userdao.show());
+		 mv.addObject("userClickindex", true);
+		 return mv;
     }
 }
 
